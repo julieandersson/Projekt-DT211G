3caabd69f3897fd3add55c459e7c9ca7b651c8bc
julieandersson <juan2301@student.miun.se>
2024-03-09 19:59:20 +0200

    adderar if och else-sats med bilder som ska visas beroende på temperatur på angiven plats

--- src/js/weather.js
+++ src/js/weather.js
@@ -31,7 +31,11 @@ function displayWeather(weatherData) {
     var temperatureCelsius = temperatureKelvin - 273.15; // Omvandla från Kelvin till Celsius
     var weatherDescription = weatherData.weather[0].description;
 
+    // Visa olika bilder beroende på väderbeskrivning på angiven plats
     var weatherIcon;
+    // Visa olika bilder beroende på temperaturen på angiven plats
+   var temperatureIcon;
+
     if (weatherDescription.includes("mulet") || weatherDescription.includes("molnigt")) {
     weatherIcon = new URL('./../images/cloudy.png', import.meta.url);
    } else if (weatherDescription.includes("soligt")) {
@@ -46,13 +50,28 @@ function displayWeather(weatherData) {
     weatherIcon = new URL('./../images/rain.png', import.meta.url);
    }
 
+    // bild som visas om temperaturen är 20 grader eller högre
+    if (temperatureCelsius >= 20) {
+        temperatureIcon = new URL('./../images/hightemp.png', import.meta.url);
+    } 
+    // bild som visas om temperaturen är mellan 10 och 20 grader
+    else if (temperatureCelsius >= 10 && temperatureCelsius < 20) {
+        temperatureIcon = new URL('./../images/mediumtemp.png', import.meta.url);
+    } 
+    // bild som visas om temperaturen är mellan 0 och 10 grader
+    else if (temperatureCelsius >= 0 && temperatureCelsius < 10) {
+        temperatureIcon = new URL('./../images/lowertemp.png', import.meta.url);
+    } 
+    // bild som visas temperaturen är -1 grader eller lägre
+    else if (temperatureCelsius < 0) {
+        temperatureIcon = new URL('./../images/minustemp.png', import.meta.url);
+    }
+
     var weatherHTML = "<h2>Väder för " + cityName + "</h2>";
     weatherHTML += "<p>Temperatur: " + temperatureCelsius.toFixed(1) + "°C</p>"; // Visa temperatur i Celsius
     weatherHTML += "<p>Väderförhållanden: " + weatherDescription + "</p>";
-
-    if (weatherIcon) {
-        weatherHTML += "<img src='" + weatherIcon + "' alt='" + weatherDescription + "'>";
-    }
+    weatherHTML += "<img src='" + weatherIcon + "' alt='Väderikon'>";
+    weatherHTML += "<img src='" + temperatureIcon + "' alt='Temperaturikon'>";
 
     weatherInfo.innerHTML = weatherHTML;
 }
