43dba6096bb0a55d8aec3688284d05637eb191a7
julieandersson <juan2301@student.miun.se>
2024-03-13 16:42:09 +0200

    hämtar antal invånare och huvudstad för angivet land, gör även manuell översättning för 4 länder till svenska

--- src/js/flags.js
+++ src/js/flags.js
@@ -1,12 +1,28 @@
 "use strict";
 
+// Skapar manuell översättning till svenska för 4 länder
+var countryTranslations = {
+    "Spanien": "Spain",
+    "Frankrike": "France",
+    "Kroatien": "Croatia",
+    "Italien": "Italy"
+};
+
+function translateCountryName(countryName) {
+    // Översätt landets namn
+    return countryTranslations[countryName] || countryName;
+}
+
 var submit = document.getElementById("submit");
 var destination = document.getElementById("destination");
 var resultDiv = document.getElementById("result");
+
 submit.addEventListener("click", () => {
     var countryName = destination.value; // Hämta landets namn från inputfältet
-    var finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    var translatedCountryName = translateCountryName(countryName); // Översätt landets namn
+    var finalURL = `https://restcountries.com/v3.1/name/${translatedCountryName}?fullText=true`;
     console.log(finalURL);
+
     fetch(finalURL)
     .then((response) => response.json())
     .then((data) => {
@@ -14,8 +30,17 @@ submit.addEventListener("click", () => {
         if (data && data.length > 0) {
             // Hämta flaggans URL från API
             var flagURL = data[0].flags.svg;
-            // Uppdatera HTML-elementet för att visa flaggan
-            resultDiv.innerHTML = `<img src="${flagURL}" alt="Flagga för ${countryName}" class="flag-img">`;
+            // Hämta antalet invånare från API
+            var population = data[0].population;
+            // Hämta landets huvudstad från API
+            var capital = data[0].capital;
+
+            // Uppdatera HTML-elementet för att visa flagga, invånare och huvudstad
+            resultDiv.innerHTML = `
+            <img src="${flagURL}" alt="Flagga för ${countryName}" class="flag-img">
+            <p>Antal invånare: ${population}</p>
+                <p>Huvudstad: ${capital}</p>
+            `;
         } else {
             // Om landet inte hittades, visa ett felmeddelande
             resultDiv.innerHTML = ""; // Visa ingen bild
@@ -26,6 +51,6 @@ submit.addEventListener("click", () => {
         // Vid fel vid anropet till API:et
         resultDiv.innerHTML = ""; // Visa ingen bild
         console.error('Det uppstod ett fel:', error);
-        alert("Ett fel uppstod vid hämtning av flaggan.");
+        alert("Ett fel uppstod vid hämtning av landet.");
     });
 });
