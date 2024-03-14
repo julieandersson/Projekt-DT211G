524c50029b0ee0bf10a97b48e15aa5fdbaa05481
julieandersson <juan2301@student.miun.se>
2024-03-14 15:41:28 +0200

    skaparen gemensam jsfil och gör en export för att importera koden i de andra jsfilerna för länderna samt tar ut datan för det specifika landet

---
+++ src/js/france.js
@@ -0,0 +1,4 @@
+import { displayCountriesTable } from './countries.js';
+
+// Kör JavaScript-koden för att visa information om Frankrike när sidan laddas
+displayCountriesTable(['France']);
---
+++ src/js/italy.js
@@ -0,0 +1,4 @@
+import { displayCountriesTable } from './countries.js';
+
+// Kör JavaScript-koden för att visa information om Italien när sidan laddas
+displayCountriesTable(['Italy']);
