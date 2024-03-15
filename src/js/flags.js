9c07a92b944be0377065d6d74384691c636b2508
julieandersson <juan2301@student.miun.se>
2024-03-15 18:34:03 +0200

    efter granskning av kod ändrar jag till try catch och await fetch på de platser det passar och bör förekomma, flyttar även autocomplete för inmatningsfältet högst upp så att det körs innan anropning av funktion för väderprognos, adderar även en alert om platsen ej är tillgänglig

--- src/js/flags.js
+++ src/js/flags.js
@@ -17,35 +17,37 @@ var submit = document.getElementById("submit");
 var destination = document.getElementById("destination");
 var resultDiv = document.getElementById("result");
 
-submit.addEventListener("click", () => {
+submit.addEventListener("click", async () => {
+  try {
     var countryName = destination.value; // Hämta landets namn från inputfältet
     var translatedCountryName = translateCountryName(countryName); // Översätt landets namn
     var finalURL = `https://restcountries.com/v3.1/name/${translatedCountryName}?fullText=true`;
     console.log(finalURL);
 
-    fetch(finalURL)
-    .then((response) => response.json())
-    .then((data) => {
-        // Kontrollera om API-svaret innehåller någon data
-        if (data && data.length > 0) {
-            // Hämta flaggans URL från API
-            var flagURL = data[0].flags.svg;
+    const response = await fetch(finalURL);
+    if (!response.ok) {
+        throw new Error('Nätverksfel: ' + response.statusText);
+    }
+    const data = await response.json();
 
-            // Uppdatera HTML-elementet för att visa flagga
-            resultDiv.innerHTML = `
-            <img src="${flagURL}" alt="Flagga för ${countryName}" class="flag-img">
-            `;
+    // Kontrollera om API-svaret innehåller någon data
+    if (data && data.length > 0) {
+        // Hämta flaggans URL från API
+        var flagURL = data[0].flags.svg;
 
-        } else {
-            // Om landet inte hittades, visa ett felmeddelande
-            resultDiv.innerHTML = ""; // Visa ingen bild
-            alert("Tyvärr kunde vi inte hämta flaggan för detta land, njut av väderinformationen istället!");
-        }
-    })
-    .catch((error) => {
-        // Vid fel vid anropet till API:et
+        // Uppdatera HTML-elementet för att visa flagga
+        resultDiv.innerHTML = `
+        <img src="${flagURL}" alt="Flagga för ${countryName}" class="flag-img">
+        `;
+
+    } else {
+        // Om landet inte hittades, visa ett felmeddelande
         resultDiv.innerHTML = ""; // Visa ingen bild
-        console.error('Det uppstod ett fel:', error);
-        alert("Ett fel uppstod vid hämtning av landet.");
-    });
+        alert("Tyvärr kunde vi inte hämta flaggan för detta land.");
+    }
+  } catch (error) {
+    console.error('Det uppstod ett fel:', error);
+    alert("Tyvärr kunde vi inte hämta flaggan för detta land.");
+  }
 });
+
--- src/js/weather.js
+++ src/js/weather.js
@@ -1,35 +1,53 @@
 "use strict";
 
-document.getElementById("searchForm").addEventListener("submit", function(event) {
+// Skapar en autocomplete för inmatningsfältet
+var autocomplete = new google.maps.places.Autocomplete(document.getElementById("destination"));
+autocomplete.addListener('place_changed', function() {
+    var place = autocomplete.getPlace();
+    if (!place.geometry || !place.formatted_address) {
+        // Om platsen inte finns
+        alert("Vänligen ange en giltig plats.");
+        return;
+    }
+    getWeatherForecast(place.formatted_address);
+});
+
+document.getElementById("searchForm").addEventListener("submit", async function(event) {
     event.preventDefault(); 
 
     var destination = document.getElementById("destination").value;
-    getWeatherForecast(destination);
-
-    // Dölj animering på startsidan genom att ta bort klassen "show"
-    document.getElementById("indexpic").classList.remove("show");
+    try {
+        await getWeatherForecast(destination);
+        // Dölj animering på startsidan genom att ta bort klassen "show"
+        document.getElementById("indexpic").classList.remove("show");
+    } catch (error) {
+        console.error('Det uppstod ett fel:', error);
+        alert("Väderprognos ej tillgänglig för angiven plats.");
+    }
 });
 
-function getWeatherForecast(destination) {
+async function getWeatherForecast(destination) {
     var apiKey = '1741ff0be3549e9c58cf88c63b2d789a'; 
     var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${destination}&appid=${apiKey}&lang=sv`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            displayWeatherForecast(data, destination);
-            document.getElementById("weatherInfo").style.display = "block";
-            document.getElementById("travelPictures").querySelector("p").style.display = "block";
-            // Hämta alla <picture>-element inom #travelPictures
-            var pictures = document.getElementById("travelPictures").querySelectorAll("picture");
-            // Loopa över varje <picture>-element och lägg till klassen visible
-            pictures.forEach(function(picture) {
-                picture.classList.add("visible");
-            });
-        })
-        .catch(error => {
-            console.log('Det uppstod ett fel:', error);
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('Nätverksfel: ' + response.statusText);
+        }
+        const data = await response.json();
+        displayWeatherForecast(data, destination);
+        document.getElementById("weatherInfo").style.display = "block";
+        document.getElementById("travelPictures").querySelector("p").style.display = "block";
+        // Hämta alla <picture>-element inom #travelPictures
+        var pictures = document.getElementById("travelPictures").querySelectorAll("picture");
+        // Loopa över varje <picture>-element och lägg till klassen visible
+        pictures.forEach(function(picture) {
+            picture.classList.add("visible");
         });
+    } catch (error) {
+        throw new Error('Det uppstod ett fel:', error);
+    }
 }
 
 function displayWeatherForecast(weatherData, destination) {
@@ -42,90 +60,94 @@ function displayWeatherForecast(weatherData, destination) {
 
     // Filtrera ut den första prognosen för varje dag
     var dailyForecasts = {};
-    weatherData.list.forEach(function(forecast) {
-        var date = new Date(forecast.dt * 1000).toLocaleDateString();
-        if (!dailyForecasts[date]) {
-            dailyForecasts[date] = [];
-        }
-        dailyForecasts[date].push(forecast);
-    });
+    try {
+        weatherData.list.forEach(function(forecast) {
+            var date = new Date(forecast.dt * 1000).toLocaleDateString();
+            if (!dailyForecasts[date]) {
+                dailyForecasts[date] = [];
+            }
+            dailyForecasts[date].push(forecast);
+        });
 
-    Object.entries(dailyForecasts).forEach(function([date, forecasts]) {
-        var table = document.createElement("table");
-        var tableHeader = document.createElement("thead");
-        var tableBody = document.createElement("tbody");
+        Object.entries(dailyForecasts).forEach(function([date, forecasts]) {
+            var table = document.createElement("table");
+            var tableHeader = document.createElement("thead");
+            var tableBody = document.createElement("tbody");
 
-        var headerRow = tableHeader.insertRow();
-        var dateHeader = document.createElement("th");
-        dateHeader.textContent = date;
-        headerRow.appendChild(dateHeader);
+            var headerRow = tableHeader.insertRow();
+            var dateHeader = document.createElement("th");
+            dateHeader.textContent = date;
+            headerRow.appendChild(dateHeader);
 
-        forecasts.forEach(function(forecast) {
-            var row = tableBody.insertRow();
-            var timeCell = row.insertCell();
-            var temperatureCell = row.insertCell();
-            var weatherCell = row.insertCell();
+            forecasts.forEach(function(forecast) {
+                var row = tableBody.insertRow();
+                var timeCell = row.insertCell();
+                var temperatureCell = row.insertCell();
+                var weatherCell = row.insertCell();
 
-        var forecastTime = new Date(forecast.dt * 1000); // Omvandla från sekunder till millisekunder och skapa ett datumobjekt
-        timeCell.textContent = forecastTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+                var forecastTime = new Date(forecast.dt * 1000); // Omvandla från sekunder till millisekunder och skapa ett datumobjekt
+                timeCell.textContent = forecastTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
 
-        var temperatureKelvin = forecast.main.temp;
-        var temperatureCelsius = temperatureKelvin - 273.15; // Omvandla från Kelvin till Celsius
-        temperatureCell.textContent = temperatureCelsius.toFixed(1) + "°C";
+                var temperatureKelvin = forecast.main.temp;
+                var temperatureCelsius = temperatureKelvin - 273.15; // Omvandla från Kelvin till Celsius
+                temperatureCell.textContent = temperatureCelsius.toFixed(1) + "°C";
 
-        var weatherDescription = forecast.weather[0].description;
-        weatherCell.textContent = weatherDescription;
+                var weatherDescription = forecast.weather[0].description;
+                weatherCell.textContent = weatherDescription;
 
-       // Visa olika bilder beroende på väderbeskrivning på angiven plats
-       var weatherIcon;
-       // Visa olika bilder beroende på temperaturen på angiven plats
-       var temperatureIcon; 
+                // Visa olika bilder beroende på väderbeskrivning på angiven plats
+                var weatherIcon;
+                // Visa olika bilder beroende på temperaturen på angiven plats
+                var temperatureIcon; 
 
-       if (weatherDescription.includes("mulet") || weatherDescription.includes("molnigt") || weatherDescription.includes("lätt molnighet")) {
-        weatherIcon = new URL('./../images/cloudy.png', import.meta.url);
-       } else if (weatherDescription.includes("soligt")) {
-        weatherIcon = new URL('./../images/sunny.png', import.meta.url);
-       } else if (weatherDescription.includes("klar himmel")) {
-        weatherIcon = new URL('./../images/clear.png', import.meta.url);
-       } else if (weatherDescription.includes("snö")) {
-        weatherIcon = new URL('./../images/snow.png', import.meta.url);
-       } else if (weatherDescription.includes("växlande molnighet")) {
-        weatherIcon = new URL('./../images/mixedclouds.png', import.meta.url);
-       } else if (weatherDescription.includes("lätt regn") || weatherDescription.includes("regn")) {
-        weatherIcon = new URL('./../images/rain.png', import.meta.url);
-       }
-    
-        // bild som visas om temperaturen är 20 grader eller högre
-        if (temperatureCelsius >= 20) {
-            temperatureIcon = new URL('./../images/hightemp.png', import.meta.url);
-        } 
-        // bild som visas om temperaturen är mellan 10 och 20 grader
-        else if (temperatureCelsius >= 10 && temperatureCelsius < 20) {
-            temperatureIcon = new URL('./../images/mediumtemp.png', import.meta.url);
-        } 
-        // bild som visas om temperaturen är mellan 0 och 10 grader
-        else if (temperatureCelsius >= 0 && temperatureCelsius < 10) {
-            temperatureIcon = new URL('./../images/lowertemp.png', import.meta.url);
-        } 
-        // bild som visas temperaturen är -1 grader eller lägre
-        else if (temperatureCelsius < 0) {
-            temperatureIcon = new URL('./../images/minustemp.png', import.meta.url);
-        }
+                if (weatherDescription.includes("mulet") || weatherDescription.includes("molnigt") || weatherDescription.includes("lätt molnighet")) {
+                    weatherIcon = new URL('./../images/cloudy.png', import.meta.url);
+                } else if (weatherDescription.includes("soligt")) {
+                    weatherIcon = new URL('./../images/sunny.png', import.meta.url);
+                } else if (weatherDescription.includes("klar himmel")) {
+                    weatherIcon = new URL('./../images/clear.png', import.meta.url);
+                } else if (weatherDescription.includes("snö")) {
+                    weatherIcon = new URL('./../images/snow.png', import.meta.url);
+                } else if (weatherDescription.includes("växlande molnighet")) {
+                    weatherIcon = new URL('./../images/mixedclouds.png', import.meta.url);
+                } else if (weatherDescription.includes("lätt regn") || weatherDescription.includes("regn")) {
+                    weatherIcon = new URL('./../images/rain.png', import.meta.url);
+                }
+                
+                // bild som visas om temperaturen är 20 grader eller högre
+                if (temperatureCelsius >= 20) {
+                    temperatureIcon = new URL('./../images/hightemp.png', import.meta.url);
+                } 
+                // bild som visas om temperaturen är mellan 10 och 20 grader
+                else if (temperatureCelsius >= 10 && temperatureCelsius < 20) {
+                    temperatureIcon = new URL('./../images/mediumtemp.png', import.meta.url);
+                } 
+                // bild som visas om temperaturen är mellan 0 och 10 grader
+                else if (temperatureCelsius >= 0 && temperatureCelsius < 10) {
+                    temperatureIcon = new URL('./../images/lowertemp.png', import.meta.url);
+                } 
+                // bild som visas temperaturen är -1 grader eller lägre
+                else if (temperatureCelsius < 0) {
+                    temperatureIcon = new URL('./../images/minustemp.png', import.meta.url);
+                }
 
-        var iconHTML = "<img src='" + weatherIcon + "' alt='Vädersymbol' style='width: 70px; height: 70px;' class='weather-icon'>";
-        iconHTML += "<img src='" + temperatureIcon + "' alt='Temperaturikon' style='width: 70px; height: 70px;' class='temperature-icon'>";
+                var iconHTML = "<img src='" + weatherIcon + "' alt='Vädersymbol' style='width: 70px; height: 70px;' class='weather-icon'>";
+                iconHTML += "<img src='" + temperatureIcon + "' alt='Temperaturikon' style='width: 70px; height: 70px;' class='temperature-icon'>";
 
-        var iconCell = row.insertCell();
-        iconCell.innerHTML = iconHTML;
-    });
+                var iconCell = row.insertCell();
+                iconCell.innerHTML = iconHTML;
+            });
 
-    table.appendChild(tableHeader);
-    table.appendChild(tableBody);
-    weatherInfo.appendChild(table);
-});
+            table.appendChild(tableHeader);
+            table.appendChild(tableBody);
+            weatherInfo.appendChild(table);
+        });
+    } catch (error) {
+        console.error('Det uppstod ett fel:', error);
+    }
 }
 
-// Skapar en autocomplete för inmatningsfältet
-var autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById("destination")
-);
+
+
+
+
